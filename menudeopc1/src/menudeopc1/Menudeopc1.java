4855d98e468ca009f313a6bdf263b9cdffa2ec78
lilianaayala5824-lgtm <liliana.ayala5824@utc.edu.ec>
2026-06-07 16:32:27 -0500

    cambio  13 LILI

--- menudeopc1/src/menudeopc1/Menudeopc1.java
+++ menudeopc1/src/menudeopc1/Menudeopc1.java
@@ -164,6 +164,8 @@ public static void ejercicio12(){
     int n=sc.nextInt();
     int a=0,b=1;
     for (int i = 1; i <= n; i++) {
+        System.out.println(a+"");
+        int c= a+b;
         
     }
     
